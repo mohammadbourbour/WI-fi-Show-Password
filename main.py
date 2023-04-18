d4a29d2a611a26367bd15c2a56764631035b16ae
Mohammad.bourbour <mohammadboorboor110@gmail.com>
2023-04-19 02:18:38 +0330

    Update main.py
    Just a little change on interface...

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """
-This Script will show the saved Wi-Fi password's on target system
+WI-Fi Show Passwords
 
 Made by : Mohammad boorboor
 Email : m.boorboor315@gmail.com
+github : github.com/mohammadbourbour
 
 """
 
@@ -14,7 +15,7 @@ import pyfiglet
 
 
 def show_wifi_passwords():
-    # My Informations
+    # My information
     email = "m.boorboor315@gmail.com"
     name = "Mohammad boorboor"
 
@@ -28,7 +29,7 @@ def show_wifi_passwords():
     print(f"Made By : {name} \nEmail : {email}\n")
 
     # prompt user to proceed
-    input("Press Enter to continue...")
+    input("Press Enter to continue...\n\n")
 
     # get list of saved Wi-Fi profiles
     try:
@@ -63,16 +64,18 @@ def show_wifi_passwords():
         except IndexError:
             ssid_passwords[profile] = ""
 
-    # print results
-    print("{:<30}|  {:<}".format("SSID", "Password"))
-    print("-" * 45)
+    # print results in a cool hacker style
+    print("\033[1;32m{:<30}|\033[1;31m  {:<}\033[0m".format("SSID", "Password"))
+    print("\033[1;36m" + "-" * 45 + "\033[0m")
     for ssid, password in ssid_passwords.items():
-        print("{:<30}|  {:<}".format(ssid, password))
+        print("\033[1;32m{:<30}|\033[1;31m  {:<}\033[0m".format(ssid, password))
 
-    # prompt user to exit
-    input("\nPress Enter to exit...")
+    # prompt user to exit in a mysterious way
+    print("\n\033[1;36mHIT ENTER TO EXIT LIKE A HACKER...\033[0m")
 
-#Execute the program
+
+
+
 if __name__ == '__main__':
     show_wifi_passwords()
-    
+    input()
